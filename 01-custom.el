ebaa9c199bd5621288e385a4a0a44ede6f519030
Marco Paolo Valerio Vezzoli <mvezzoli@micron.com>
2021-06-14 17:08:36 +0200

    native fix

--- 01-custom.el
+++ 01-custom.el
@@ -4,9 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
- '(package-selected-packages
-   (quote
-    (window-numbering yaml-imenu yaml-mode 0x0 0blayout origami yafolding use-package))))
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
+ '(straight-disable-native-compilation t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
