538f1cdbcae87e4aebc37e328a4c03ddab22991d
Marco Paolo Valerio Vezzoli <mvezzoli@micron.com>
2021-06-14 16:10:45 +0200

    updated customization

--- 01-custom.el
+++ 01-custom.el
@@ -4,9 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
  '(package-selected-packages
-   (quote
-    (window-numbering yaml-imenu yaml-mode 0x0 0blayout origami yafolding use-package))))
+   '(window-numbering yaml-imenu yaml-mode 0x0 0blayout origami yafolding use-package))
+ '(safe-local-variable-values '((conda-project-env-name . "django2-python36-arda"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
